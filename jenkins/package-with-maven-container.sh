2565b30bb74bcbb50fd1bb1bde414e99a2e4ae55
DennisSalmazs <skrn.sdet@gmail.com>
2025-12-16 00:40:10 +0000

    added qa automation pipeline for dev

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,1 +1,3 @@
-docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.8-openjdk-11 mvn clean package
+#!/bin/bash
+WORKDIR="${WORKSPACE:-$(pwd)}"
+docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKDIR:/app -w /app maven:3.8-openjdk-11 mvn clean package

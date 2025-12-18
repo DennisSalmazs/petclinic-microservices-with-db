a99f86e9a16f42077a74cb9f1c5c5cd1d7283ec5
DennisSalmazs <skrn.sdet@gmail.com>
2025-12-18 00:01:26 +0000

    update

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-WORKDIR="${WORKSPACE:-$(pwd)}"
-docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKDIR:/app -w /app maven:3.8-openjdk-11 mvn clean package
+# WORKDIR="${WORKSPACE:-$(pwd)}"
+# docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKDIR:/app -w /app maven:3.8-openjdk-11 mvn clean package
+docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.8-openjdk-11 mvn clean package
